0e29834d74b6fc6c109b06191a4923a6c0cc2517
Andrej Kabachnik <andrej_ka@gmx.de>
2020-01-09 18:06:20 +0100

    BUMP version to 1.4.7.9

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,5 +1,5 @@
 <?php
-$modx_version      = '1.4.7.8'; // Current version number
+$modx_version      = '1.4.7.9'; // Current version number
 $modx_release_date = 'June 04, 2019'; // Date of release
 $modx_branch       = 'Evolution'; // Codebase name
 $modx_full_appname = "{$modx_branch} {$modx_version} ({$modx_release_date})";
